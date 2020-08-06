ad33b157d88b84562833abee6b2410ff9add6037
kani0798 <arstanbaevakani@gmail.com>
2020-08-06 13:41:19 +0600

    Add hosts.

--- makers_test/settings.py
+++ makers_test/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['http://localhost', 'http://127.0.0.1', 'https://demo-kanidjangotest.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'demo-kanidjangotest.herokuapp.com']
 
 
 # Application definition
