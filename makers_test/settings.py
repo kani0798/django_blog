47051f41c05bcea9aca36455159b585413f28fbd
kani0798 <arstanbaevakani@gmail.com>
2020-08-06 13:51:33 +0600

    Fixed media root.

--- makers_test/settings.py
+++ makers_test/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'accounts/login'
